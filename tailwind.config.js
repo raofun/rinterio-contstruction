ce9a698991be2bdeec1731e28f6e2dd4fe65743b
raofun_ <raofunazad851@gmail.com>
2025-10-26 04:47:15 +0600

    HEader and About us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
         extend: {
             colors: {
                 clifford: '#da373d',
-                'rin-primary': 'rgba(182, 140, 90, 1)',
-                'rin-bg': 'rgba(182, 140, 90, 0.1)',
-                'dark-3': '#707070',
+                'rin-primary': '#ABEF5F',
+                'rin-secondary': 'rgba(171, 239, 95, 0.50)',
+                'rin-bg': '#F3F3F3',
+                'rin-bg-2': '#F4F4F4',
+                'dark-3': '#494949',
+                'dark-4': '#737373',
 
             },
         },
