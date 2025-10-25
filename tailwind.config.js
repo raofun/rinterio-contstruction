d1349f84a2e263cc621b6deaa8e202e3cb741dc3
raofun_ <raofunazad851@gmail.com>
2025-10-26 02:39:25 +0600

    initial files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'rin-primary': 'rgba(182, 140, 90, 1)',
+                'rin-bg': 'rgba(182, 140, 90, 0.1)',
+                'dark-3': '#707070',
+
+            },
+        },
+    },
+    plugins: [],
+}
